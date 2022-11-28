f9c73cc150d310289d36fe28f9ab59773b6c836f
Jmabeg <josemariabernues@gmail.com>
2022-11-28 11:00:12 +0100

    Manager added

---
+++ dev.project.backend/src/main/java/solera/berny/dev/project/backend/model/Manager.java
@@ -0,0 +1,34 @@
+package solera.berny.dev.project.backend.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Manager {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String password;
+    private String email;
+
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "manager")
+    private List<Restaurant> restaurants;
+
+}
